d280ab8624b63b6723521a678e72e0e4ef3c5838
dafma <mar_tp@hotmail.es>
2016-12-15 21:55:53 -0600

    class active in navbar

--- StripePaymenDjango/urls.py
+++ StripePaymenDjango/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from profiles.views import home, about
+from django.conf import settings
+from django.conf.urls.static import static
+from contact import contact_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
+    url(r'^about/', about, name='about'),
+    url(r'^contact/', contact_views.contact, name='contact'),
 ]
+"""
+if settings.DEBUG:
+	urlpatterns += static(settings, STATIC)
+"""
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+	name = models.CharField(max_length=120)
+	description = models.TextField(default='text description default')
+	location = models.CharField(max_length=120)
+	job = models.CharField(max_length=120, null=True)
+
+	def __str__(self):
+		return self.name
